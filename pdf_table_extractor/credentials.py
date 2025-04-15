0e6484ed720735a6d0b140137f740fbef7e605df
quesabalovin <lovin.quesaba@gmail.com>
2025-04-16 06:33:33 +0800

    Update credentials.py

--- pdf_table_extractor/credentials.py
+++ pdf_table_extractor/credentials.py
@@ -1,9 +1,14 @@
 # credentials.py
 
 USERS = {
-    "freetrial@example.com": {"password": "trialpass", "credits": 3},
-    "test@example.com": {"password": "test123", "credits": 25},
-    "admin@example.com": {"password": "admin456", "credits": 100},
-    "jane@example.com": {"password": "jane123", "credits": 50},
-    "john@example.com": {"password": "john456", "credits": 100}
+    "tableextractorizexpt@loq": {"password": "ov08c2ct", "credits": 100},
+    "tableextractorbtqh72@loq": {"password": "7infr6v9", "credits": 100},
+    "tableextractor7sxh7k@loq": {"password": "oyfkdmbo", "credits": 100},
+    "tableextractorohpun8@loq": {"password": "30g9q7fb", "credits": 100},
+    "tableextractorm1t62s@loq": {"password": "twrpi67c", "credits": 100},
+    "tableextractork2b8d5@loq": {"password": "r0nm2u94", "credits": 100},
+    "tableextractor9reci3@loq": {"password": "tp8lbg8j", "credits": 100},
+    "tableextractorndadep@loq": {"password": "b8kaf99a", "credits": 100},
+    "tableextractor6oyo4k@loq": {"password": "93go1qw3", "credits": 100},
+    "tableextractor838lxx@loq": {"password": "7yeet3j1", "credits": 100}
 }
